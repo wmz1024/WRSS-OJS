7bfc0eb445bbdcd3af2597bbb388188c14310b4c
Wmz1024 <wmzwsa@gmail.com>
2023-06-09 12:35:18 +0800

    🎆 测试功能 SW缓存

--- sw.js
+++ sw.js
@@ -2,6 +2,35 @@ self.addEventListener('message', (event) => {
   ipu = event.data.ipu
 });
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/main.js',
+        '/assets/main.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== 'app-cache';
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
+
 self.addEventListener('fetch', function(event) {
   if (//event.request.headers.get('Content-Type').indexOf('image') !== -1 || 
   /https?:\/\/.*\.(jpe?g|png|gif|bmp|webp)/i.test(event.request.url) 
@@ -21,6 +50,30 @@ self.addEventListener('fetch', function(event) {
     )
   }}
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      var request = event.request.clone();
+      return fetch(request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var responseToCache = response.clone();
+        caches.open('app-cache').then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(function(error) {
+        console.error(error);
+      });
+    })
+  );
+});
+
 
 const handle = async(req)=>{
   return fetch(req)
